6fd07c7bce64af345fcec101992e86f5075d5670
Pavel Toporov <pavel.toporov@raiffeisen.ru>
2020-10-30 20:55:31 +0300

    [NF] create 4th home project with yandex practicum

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,30 @@
-const PROFILE = document.querySelector('.profile');
-const EDIT_PROFILE_BUTTON = PROFILE.querySelector('.profile__setting');
-const PROFILE_TITLE_TEXT = PROFILE.querySelector('.profile__title-text')
-const PROFILE__SPEC = PROFILE.querySelector('.profile__specialisation')
-const POPUP = document.querySelector('.popup')
-const POPUP_CLOSE_BUTTON = POPUP.querySelector('.popup__close')
-const POPUP_SAVE_BUTTON = POPUP.querySelector('.popup__save-button')
-const POPUP_EDIT_FORM = POPUP.querySelector('.popup__edit-form')
-const POPUP_INPUT_TITLE = POPUP_EDIT_FORM.querySelector('.popup__input_title')
-const POPUP_INPUT_SPEC = POPUP_EDIT_FORM.querySelector('.popup__input_spec')
-
-
-EDIT_PROFILE_BUTTON.addEventListener('mouseup', openPopup);
-POPUP_CLOSE_BUTTON.addEventListener('mousedown', closePopup)
-POPUP_EDIT_FORM.addEventListener('submit', saveProfileChanges)
+const profile = document.querySelector('.profile');
+const editProfileButton = profile.querySelector('.profile__setting');
+const profileTitleText = profile.querySelector('.profile__title-text')
+const profileSpec = profile.querySelector('.profile__specialisation')
+const popup = document.querySelector('.popup')
+const popupCloseButton = popup.querySelector('.popup__close')
+const popupEditForm = popup.querySelector('.popup__edit-form')
+const popupInputTitle = popupEditForm.querySelector('.popup__input_title')
+const popupInputSpec = popupEditForm.querySelector('.popup__input_spec')
 
 function openPopup() {
-    POPUP.classList.add('popup_opened');
-    POPUP_INPUT_TITLE.value = PROFILE_TITLE_TEXT.textContent;
-    POPUP_INPUT_SPEC.value = PROFILE__SPEC.textContent;
+    popup.classList.add('popup_opened');
+    popupInputTitle.value = profileTitleText.textContent;
+    popupInputSpec.value = profileSpec.textContent;
 }
 
 function saveProfileChanges(event) {
     event.preventDefault()
-    PROFILE_TITLE_TEXT.textContent = POPUP_INPUT_TITLE.value;
-    PROFILE__SPEC.textContent = POPUP_INPUT_SPEC.value;
+    profileTitleText.textContent = popupInputTitle.value;
+    profileSpec.textContent = popupInputSpec.value;
     closePopup()
 }
 
 function closePopup() {
-    POPUP.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened');
 }
 
-
-console.log(EDIT_PROFILE_BUTTON)
+editProfileButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup)
+popupEditForm.addEventListener('submit', saveProfileChanges)
